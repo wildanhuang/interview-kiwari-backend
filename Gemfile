5806611d3047230f902592d180003e28e3374af0
Wildan <wildan.p@kiranatama.com>
2019-07-05 20:06:09 +0700

    Updating API funtions

--- Gemfile
+++ Gemfile
@@ -35,6 +35,9 @@ gem 'jbuilder', '~> 2.5'
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.1.0', require: false
 
+# Used for respond_
+gem 'responders'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -1,2 +1,66 @@
 class Api::V1::ProductsController < ApplicationController
+  respond_to :json
+
+  def index
+    products = Product.all.collect{ |product| collect_data(product) }
+    render json: {code: 200, msg: "Success", data: products}
+  end
+
+  def show
+    product = Product.find(params[:id]) rescue nil
+    if product
+      success_respond(product)
+    else
+      render json: {code: 404, msg: "Failed: data not found"}
+    end
+  end
+
+  def create
+    product = Product.new(product_params)
+
+    if product.valid? && product.save
+      success_respond(product)
+    else
+      render json: {code: 404, msg: "Failed: data not found"}
+    end
+  end
+
+  def update
+    product = Product.find(params[:id]) rescue nil
+    data = product_params
+
+    if product.present? && data.present? && product.update_attributes(data)
+      success_respond(product)
+    else
+      render json: {code: 404, msg: "Failed: data not found"}
+    end
+  end
+
+  def destroy
+    product = Product.find(params[:id]) rescue nil
+
+    if product.present? && product.destroy
+      render json: {code: 200, msg: "Success", data: []}
+    else
+      render json: {code: 404, msg: "Failed: data not found"}
+    end
+  end
+
+  def success_respond(product)
+    render json: {code: 200, msg: "Success", data: collect_data(product)}
+  end
+
+  private
+    def collect_data(record)
+      { name: record.name, price: record.price, image: record.image }
+    end
+
+    def product_params
+      data = {}
+      data.merge!({name: params[:name]}) if params[:name].present?
+      data.merge!({price: params[:price]}) if params[:price].present?
+      data.merge!({image: params[:image]}) if params[:image].present?
+
+      return data
+    end
 end
